5ababb3492247797112bb036daa007d1663629fb
Alex Turner <alexrmturner@gmail.com>
2026-05-27 22:27:16 -0700

    Build and publish fixes

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,16 +1,25 @@
 import { defineConfig } from "tsup"
 
-export default defineConfig({
-  entry: [
-    "src/index.ts",
-    "src/drivers/postgres-js.driver.ts",
-    "src/drivers/better-sqlite3.driver.ts",
-    "src/drivers/node-sqlite.driver.ts",
-    "bin/db-evolutions.ts",
-  ],
-  format: ["cjs", "esm"],
-  dts: true,
-  clean: true,
-  sourcemap: true,
-  target: "node18",
-})
+export default defineConfig([
+  {
+    entry: [
+      "src/index.ts",
+      "src/drivers/postgres-js.driver.ts",
+      "src/drivers/better-sqlite3.driver.ts",
+      "src/drivers/node-sqlite.driver.ts",
+    ],
+    format: ["cjs", "esm"],
+    dts: true,
+    clean: true,
+    sourcemap: true,
+    target: "node18",
+  },
+  {
+    entry: ["bin/db-evolutions.ts"],
+    outDir: "dist/bin",
+    format: ["cjs", "esm"],
+    dts: false,
+    sourcemap: true,
+    target: "node18",
+  }
+])
